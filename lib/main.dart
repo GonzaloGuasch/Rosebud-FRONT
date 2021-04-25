b5cb11daa1d9ceed26d761394a5e2ebde252e449
GonzaloGuasch <gonzaloguasch@outlook.com>
2021-04-25 12:05:16 -0400

    Agrego barra de navegacion [DONE]

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rosebud_front/widgets/movie/movie_container.dart';
-
 import 'widgets/NavigationBar.dart';
 
 void main() {
@@ -19,45 +17,7 @@ class RosebudApp extends StatelessWidget {
   }
 }
 
-class HomeChoice extends StatefulWidget {
-  @override
-  _HomeChoiceState createState() => _HomeChoiceState();
-}
 
-class _HomeChoiceState extends State<HomeChoice> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-       body: Center(
-            child:
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text("ELEGI LA CATEGORIA",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                  IconButton(
-                    icon: Icon(Icons.movie, size: 50),
-                    tooltip: 'Movie section',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => MovieContainer()),
-                      );
-                    },
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.music_note, size: 50),
-                    tooltip: 'Music section',
-                    onPressed: () {
-                    },
-                  ),
-                ],
-              )
-         ),
-    );
-  }
-}
 
 
 
---
+++ lib/widgets/Home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:rosebud_front/widgets/movie/movie_container.dart';
+
+class HomeChoice extends StatefulWidget {
+  @override
+  _HomeChoiceState createState() => _HomeChoiceState();
+}
+
+class _HomeChoiceState extends State<HomeChoice> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("ELEGI LA CATEGORIA",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+              IconButton(
+                icon: Icon(Icons.movie, size: 50),
+                tooltip: 'Movie section',
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MovieContainer()),
+                  );
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.music_note, size: 50),
+                tooltip: 'Music section',
+                onPressed: () {
+                },
+              ),
+            ],
+          )
+      ),
+    );
+  }
+}
