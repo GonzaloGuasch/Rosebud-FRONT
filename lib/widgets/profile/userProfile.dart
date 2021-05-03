9a3b9582a3bafe92cb95adbd376e01d3f7ef92f7
GonzaloGuasch <gonzaloguasch@outlook.com>
2021-05-03 14:38:23 -0400

    user profile con las secciones del usuario[WIP]

--- lib/widgets/profile/userProfile.dart
+++ lib/widgets/profile/userProfile.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/cupertino.dart';
 
+class DataProfile extends StatelessWidget {
+ final String amount;
+ final String category;
+ const DataProfile(this.amount, this.category, {Key key}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(this.amount, style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold)),
+        Text(this.category, style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold))
+      ],
+    );
+  }
+}
+
 class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('PERFIL USUARIO'),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 30.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            DataProfile('1', 'vistas'),
+            DataProfile('0', 'seguidores'),
+            DataProfile('0', 'seguidos'),
+          ],
+        ),
+      ),
     );
   }
 }
